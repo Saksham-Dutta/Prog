06d89c0d312dbe3629f5a00e78ada223de2e99b9
Saksham-Dutta <dutta.saksham04@gmail.com>
2026-06-05 21:37:00 +0530

    Day 3 assignment is uploaded.

--- Day 3/b.py
+++ Day 3/b.py
@@ -1,1 +1,10 @@
 # Write a program to Print prime numbers in a range.
+n = int(input("Enter a number: ")) # 21
+for i in range(2,  n + 1 ): # 2,3,4,5,6,7,8,.......21 (inclusive)
+    isPrime = True
+    for j in range(2, i):
+        if i % j == 0:
+            isPrime = False
+            # break
+    if isPrime:
+        print(i)
