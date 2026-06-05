50661e1a087915825f327d85b45e10204631eb73
Saksham-Dutta <dutta.saksham04@gmail.com>
2026-06-05 20:37:33 +0530

    Day 3 assignment is uploaded.

--- Day 3/a.py
+++ Day 3/a.py
@@ -1,1 +1,15 @@
 # Write a program to Check whether a number is prime.
+
+a = int(input("Enter a number: "))
+
+isPrime = True
+
+for i in range(2, a):
+    if a % i == 0:
+        isPrime = False
+        break
+
+if isPrime:
+    print("Prime number.")
+else:
+    print("Not a prime number.")
