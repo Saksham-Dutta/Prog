e9d64efd11a8761a4abbb11c7749cf7a02e95dec
Saksham-Dutta <dutta.saksham04@gmail.com>
2026-06-07 21:49:51 +0530

    Day 3 assignment is uploaded.

--- Day 3/c.py
+++ Day 3/c.py
@@ -1,1 +1,15 @@
-# Write a program to Find GCD of two numbers.
+# Write a program to Find GCD of two numbers
+def returnFactorSet(num):
+    factors = set()
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.add(i)
+    return factors
+
+numOne = int(input("Enter 1st number: "))
+numTwo = int(input("Enter 2nd number: "))
+
+factorsOfNumOne = returnFactorSet(numOne)
+factorsOfNumTwo = returnFactorSet(numTwo)
+
+print(f"GCD: {max(factorsOfNumOne.intersection(factorsOfNumTwo))}")
--- Day 3/d.py
+++ Day 3/d.py
@@ -1,1 +1,22 @@
 # Write a program to Find LCM of two numbers.
+def returnFactorSet(num):
+    factors = set()
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.add(i)
+    return factors
+
+
+numOne = int(input("Enter 1st number: "))
+numTwo = int(input("Enter 2nd number: "))
+
+factorsOfNumOne = returnFactorSet(numOne)
+factorsOfNumTwo = returnFactorSet(numTwo)
+
+gcd = max(factorsOfNumOne.intersection(factorsOfNumTwo))
+
+product = numOne * numTwo
+
+lcm = product / gcd
+
+print(f"LCM: {lcm}")
